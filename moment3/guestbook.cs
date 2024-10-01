36e026b36f2560a544815c7c81bae91604fcf85f
CalleAlexanderson <calle.alexanderson@telia.com>
2024-10-01 20:50:47 +0200

    fixade så det kontrolleras att json filen finns för att undvika error

--- moment3/guestbook.cs
+++ moment3/guestbook.cs
@@ -5,15 +5,18 @@ namespace Guestspace
     {
         public static Guest[] getOldBook()
         {
-            string oldbook = File.ReadAllText(@"guestbook.json");
-            if (oldbook != "") //kollar så json filen är blank
+            if (File.Exists(@"guestbook.json"))
             {
-                Guest[] oldbookArr = JsonSerializer.Deserialize<Guest[]>(oldbook); //omvandlar json datan till array av guests
-                for (int i = 0; i < oldbookArr.Length; i++)
+                string oldbook = File.ReadAllText(@"guestbook.json");
+                if (oldbook != "") //kollar om json filen är blank
                 {
-                    Console.WriteLine($"[{i+1}] {oldbookArr[i].Name} - {oldbookArr[i].Message}"); //skriver ut alla inlägg i boken
+                    Guest[] oldbookArr = JsonSerializer.Deserialize<Guest[]>(oldbook); //omvandlar json datan till array av guests
+                    for (int i = 0; i < oldbookArr.Length; i++)
+                    {
+                        Console.WriteLine($"[{i + 1}] {oldbookArr[i].Name} - {oldbookArr[i].Message}"); //skriver ut alla inlägg i boken
+                    }
+                    return oldbookArr; //returnerar en array med de olika guest objekten
                 }
-                return oldbookArr; //returnerar en array med de olika guest objekten
             }
             return []; // om json är blank returneras en tom array
         }
@@ -49,19 +52,20 @@ namespace Guestspace
             Console.WriteLine("Skriv numret på det inlägg som ska tas bort");
             string inputIndex = Console.ReadLine();
             bool isNr = int.TryParse(inputIndex, out int nr); //kollar så input är ett nummer
-            int length = bookList.Count -1; //gör miuns 1 eftersom index startar på 0
-            if (isNr) 
+            int length = bookList.Count - 1; //gör miuns 1 eftersom index startar på 0
+            if (isNr)
             {
                 if (nr <= length) //om nummer som skrivit är mindre eller lika med length tas indexet bort
                 {
-                    bookList.RemoveAt(nr-1);
+                    bookList.RemoveAt(nr - 1);
                     updateBook(JsonSerializer.Serialize(bookList)); //konverterar listan till json och skickar till updateBook
                 }
             }
-            
+
         }
 
-        private static void updateBook (string newBook){
+        private static void updateBook(string newBook)
+        {
             File.WriteAllText(@"guestbook.json", newBook); //skriver in den nya json datan i filen guestbook.json
         }
     }
